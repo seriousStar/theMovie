aa4d02a872515911ed5e970bf2c17c29d0221367
Frank W <frank.w101317@gmail.com>
2022-03-25 16:45:05 -0600

    feat: integrate MovieDetail api on Details screen

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,63 +1,101 @@
 import React, {Component} from 'react';
 import {View, Text, Image, ScrollView, Linking} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {connect} from 'react-redux';
+import _ from 'lodash';
+
 import {styles} from './styles';
 import {CommonStyle} from '../../themes';
-import {convertStrToDate} from '../../utils';
+import {convertStrToDate, getImageUrl} from '../../utils';
+import {getMovieDetail} from '../../redux/actions';
+import {Indicator} from '../../components';
 
 class DetailsScreen extends Component {
   state = {
     selectedMovie: {},
   };
   componentDidMount() {
-    const {route} = this.props;
-    this.setState({selectedMovie: route.params.data});
+    this.onInitial();
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (!_.isEqual(this.props.movie.detail, prevProps.movie.detail)) {
+      this.setState({selectedMovie: this.props.movie.detail});
+    }
+  }
+
+  onInitial = async () => {
+    const {route} = this.props;
+    await this.props.getMovieDetail(route.params.data.id);
+  };
+
   goToDetails = () => {
-    Linking.canOpenURL('https://www.disneyplus.com/movies/turning-red/4mFPCXJi7N2m').then(supported => {
-      if (supported) {
-        Linking.openURL('https://www.disneyplus.com/movies/turning-red/4mFPCXJi7N2m');
-      } else {
-        console.log(`Don't know how to open URI: `);
-      }
-    });
+    const {selectedMovie} = this.state;
+    if (selectedMovie.homepage) {
+      Linking.canOpenURL(selectedMovie.homepage).then(supported => {
+        if (supported) {
+          Linking.openURL(selectedMovie.homepage);
+        } else {
+          console.log(`Don't know how to open URI: ${selectedMovie.homepage}`);
+        }
+      });
+    }
   };
   render() {
     const {selectedMovie} = this.state;
-
+    const {isFetching, configuration} = this.props;
+    console.info('configuration.images', configuration.images);
     return (
       <SafeAreaView style={CommonStyle.container} edges={['right', 'bottom', 'left']}>
-        <ScrollView>
-          <Image
-            source={{uri: `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path || selectedMovie.backdrop_path}`}}
-            style={styles.itemImg}
-          />
-          <View style={styles.mainContainer}>
-            <Text style={styles.titleText}>{selectedMovie.title}</Text>
-            <Text style={styles.descriptionText}>{selectedMovie.overview}</Text>
-            <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
-              <Text style={styles.descriptionBoldText}>Popularity: </Text>
-              <Text style={styles.descriptionText}>{selectedMovie.popularity}</Text>
+        {!isFetching && (
+          <ScrollView>
+            <Image
+              source={{
+                uri: getImageUrl(configuration.images, selectedMovie.poster_path || selectedMovie.backdrop_path, false),
+              }}
+              style={styles.itemImg}
+            />
+            <View style={styles.mainContainer}>
+              {!!selectedMovie.title && <Text style={styles.titleText}>{selectedMovie.title}</Text>}
+              {!!selectedMovie.overview && <Text style={styles.descriptionText}>{selectedMovie.overview}</Text>}
+              {!!selectedMovie.popularity && (
+                <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
+                  <Text style={styles.descriptionBoldText}>Popularity: </Text>
+                  <Text style={styles.descriptionText}>{selectedMovie.popularity}</Text>
+                </View>
+              )}
+              {!!selectedMovie.release_date && (
+                <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
+                  <Text style={styles.descriptionBoldText}>Release Date: </Text>
+                  <Text style={styles.descriptionText}>{convertStrToDate(selectedMovie.release_date)}</Text>
+                </View>
+              )}
+              {!!selectedMovie.runtime && (
+                <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
+                  <Text style={styles.descriptionBoldText}>Runtime: </Text>
+                  <Text style={styles.descriptionText}>{`${selectedMovie.runtime} min`}</Text>
+                </View>
+              )}
+              {!!selectedMovie.homepage && (
+                <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
+                  <Text style={styles.descriptionBoldText} onPress={this.goToDetails}>
+                    More Details? Go to Homepage
+                  </Text>
+                </View>
+              )}
             </View>
-            <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
-              <Text style={styles.descriptionBoldText}>Release Date: </Text>
-              <Text style={styles.descriptionText}>{convertStrToDate(selectedMovie.release_date)}</Text>
-            </View>
-            <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
-              <Text style={styles.descriptionBoldText}>Runtime: </Text>
-              <Text style={styles.descriptionText}>100 min</Text>
-            </View>
-            <View style={[CommonStyle.rowContainer, styles.descriptionContainer]}>
-              <Text style={styles.descriptionBoldText} onPress={this.goToDetails}>
-                More Details? Go to Homepage
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
+          </ScrollView>
+        )}
+        {isFetching && <Indicator />}
       </SafeAreaView>
     );
   }
 }
 
-export default DetailsScreen;
+const mapStateToProps = state => ({
+  movie: state.movie,
+  configuration: state.configuration,
+  isFetching: state.movie.isFetching,
+});
+
+export default connect(mapStateToProps, {getMovieDetail})(DetailsScreen);
--- src/services/MovieAPI.js
+++ src/services/MovieAPI.js
@@ -6,3 +6,7 @@ const {API_KEY} = AppConfig;
 export const getPopularMovies = async pageIndex => {
   return apiModel(`movie/popular?api_key=${API_KEY}&language=en-US&page=${pageIndex}`, 'GET');
 };
+
+export const getMovieDetail = async movieId => {
+  return apiModel(`movie/${movieId}?api_key=${API_KEY}&language=en-US`, 'GET');
+};
